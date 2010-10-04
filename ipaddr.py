f109f0be7233c4cae807cce0de3f2b4b4cbbc125
Daniel Harrison <harro@google.com>
2010-10-04 23:13:08 +0000

    Commentary typo fix
    git-svn-id: https://ipaddr-py.googlecode.com/svn/trunk@192 09200d28-7f98-11dd-ad27-0f66e57d2035

--- ipaddr.py
+++ ipaddr.py
@@ -915,7 +915,7 @@ class _BaseNet(_IPAddrBase):
                          version=self._version)
 
     def subnet(self, prefixlen_diff=1, new_prefix=None):
-        """Return a list of subnets, rather than an interator."""
+        """Return a list of subnets, rather than an iterator."""
         return list(self.iter_subnets(prefixlen_diff, new_prefix))
 
     def supernet(self, prefixlen_diff=1, new_prefix=None):
